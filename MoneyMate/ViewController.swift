dfcd47aebbee475759fb24e5283619596deaabca
dai duong <dai22992@gmail.com>
2025-09-29 21:18:18 +0700

    Initial Commit

---
+++ MoneyMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyMate
+//
+//  Created by Duong on 29/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MoneyMateTests/MoneyMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyMateTests.swift
+//  MoneyMateTests
+//
+//  Created by Duong on 29/9/25.
+//
+
+import Testing
+@testable import MoneyMate
+
+struct MoneyMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
